cdd498b17d7e1eb81c65c4f6ee83d2217fe46bc7
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-11 17:17:48 +0500

    imp

--- lib/models/note_model.dart
+++ lib/models/note_model.dart
@@ -1,5 +1,23 @@
 class NoteModel {
   // members
+  late String name;
+  late DateTime time;
 
-  // from json
+  // constructor
+  NoteModel({
+    required this.name,
+    required this.time,
+  });
+
+  // fromJson
+  NoteModel.fromJson(Map json) {
+    name = json["name"];
+    time = DateTime.parse(json["time"]);
+  }
+
+  // toJson
+  Map toJson() => {
+    "name": name,
+    "time": time.toIso8601String(),
+  };
 }
