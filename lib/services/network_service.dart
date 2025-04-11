ea88ba62e18e79296922496c4e97780637afeae1
IslomjonovAbdulazim <111278113+IslomjonovAbdulazim@users.noreply.github.com>
2025-04-11 17:33:15 +0500

    imp

--- lib/services/network_service.dart
+++ lib/services/network_service.dart
@@ -1,3 +1,29 @@
+import 'dart:convert';
+
+import 'package:d4_8_3/models/note_model.dart';
 import 'package:http/http.dart' as http;
 
-class NetworkService {}
+class NetworkService {
+  static const baseUrl =
+      "https://67f908e6094de2fe6ea0349e.mockapi.io/note_model";
+
+  static Future<List<NoteModel>> getAllNotes() async {
+    List<NoteModel> result = [];
+    Uri uri = Uri.parse(baseUrl);
+    final response = await http.get(uri);
+    if (response.statusCode == 200) {
+      List jsonList = List.from(jsonDecode(response.body));
+      result = jsonList.map((json) => NoteModel.fromJson(json)).toList();
+    }
+    return result;
+  }
+
+  static Future<void> createNote(NoteModel model) async {
+    Uri uri = Uri.parse(baseUrl);
+    await http.put(
+      uri,
+      body: model.toJson(),
+      headers: {"Content-Type": "application/json"},
+    );
+  }
+}
